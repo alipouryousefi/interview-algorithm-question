087ec433954608c1a635df32f6f3d1f26b846cba
Ali Py <a.pouryousefi98@gmail.com>
2023-07-19 20:08:33 +0330

    solve Two Sum problem

---
+++ Easy/Two Sum/solution.js
@@ -0,0 +1,21 @@
+function twoSum(nums, target) {
+    const numToIndexMap = {};
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+  
+      if (complement in numToIndexMap) {
+        return [numToIndexMap[complement], i];
+      }
+  
+      numToIndexMap[nums[i]] = i;
+    }
+  
+    // If no solution is found, return an empty array
+    return [];
+  }
+  
+  // Test cases
+  console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+  console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+  console.log(twoSum([3, 3], 6)); // Output: [0, 1]
