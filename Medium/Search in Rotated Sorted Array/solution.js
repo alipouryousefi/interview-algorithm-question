4cf41c90294763611efb431a2048928e9b7cf041
Ali Py <a.pouryousefi98@gmail.com>
2023-07-21 12:09:37 +0330

    solve Search in Rotated Sorted Array

---
+++ Medium/Search in Rotated Sorted Array/solution.js
@@ -0,0 +1,35 @@
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+  
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+  
+      if (nums[mid] === target) {
+        return mid;
+      }
+  
+      // Check if the left half is sorted
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target < nums[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else { // Right half is sorted
+        if (nums[mid] < target && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+  
+    return -1;
+  }
+  
+  // Example usage:
+  console.log(search([4,5,6,7,0,1,2], 0)); // Output: 4
+  console.log(search([4,5,6,7,0,1,2], 3)); // Output: -1
+  console.log(search([1], 0)); // Output: -1
+  
