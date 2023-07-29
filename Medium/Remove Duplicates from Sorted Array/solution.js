412ee91054d788d4d8c9a2b9b653d7dd4087e2e7
Ali Py <a.pouryousefi98@gmail.com>
2023-07-29 22:37:39 +0330

    solve remove duplicate from sorted array

---
+++ Medium/Remove Duplicates from Sorted Array/solution.js
@@ -0,0 +1,34 @@
+function removeDuplicates(nums) {
+    // Edge case: If the array has less than or equal to 2 elements, no duplicates to remove
+    if (nums.length <= 2) {
+      return nums.length;
+    }
+  
+    // Initialize pointers
+    let k = 2; // Pointer to keep track of the next unique element position
+    let prev = 1; // Pointer to keep track of the previous element
+    let curr = 2; // Pointer to keep track of the current element
+  
+    while (curr < nums.length) {
+      // Check if the current element is a duplicate
+      if (nums[curr] === nums[prev] && nums[curr] === nums[prev - 1]) {
+        curr++;
+      } else {
+        // If it's not a duplicate, move the unique element to its correct position
+        nums[k] = nums[curr];
+        k++;
+        prev++;
+        curr++;
+      }
+    }
+  
+    return k;
+  }
+  
+  // Test cases
+  const nums1 = [1, 1, 1, 2, 2, 3];
+  console.log(removeDuplicates(nums1)); // Output: 5, nums = [1, 1, 2, 2, 3, _]
+  
+  const nums2 = [0, 0, 1, 1, 1, 1, 2, 3, 3];
+  console.log(removeDuplicates(nums2)); // Output: 7, nums = [0, 0, 1, 1, 2, 3, 3, _, _]
+  
